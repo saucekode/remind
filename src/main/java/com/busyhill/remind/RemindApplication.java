23c8acc7b2af7265fe6475fb641b8315844c534c
saucekode <saucekoder@gmail.com>
2025-01-08 03:23:26 +0100

    reverted

--- src/main/java/com/busyhill/remind/RemindApplication.java
+++ src/main/java/com/busyhill/remind/RemindApplication.java
@@ -1,6 +1,5 @@
 package com.busyhill.remind;
 
-import com.busyhill.remind.config.OauthAccessConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,27 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(OauthAccessConfig.class)
-public class  RemindApplication implements CommandLineRunner {
-
-	private final OauthAccessConfig oauthAccessConfig;
-
-    public RemindApplication(OauthAccessConfig oauthAccessConfig) {
-        this.oauthAccessConfig = oauthAccessConfig;
-    }
+public class  RemindApplication {
 
     public static void main(String[] args) {
 		SpringApplication.run(RemindApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Logger logger = LoggerFactory.getLogger(RemindApplication.class);
-
-		logger.info("----------------------------------------");
-		logger.info("Configuration properties");
-		logger.info("   oauth.id is {}", oauthAccessConfig.getId());
-		logger.info("   oauth.secret is {}", oauthAccessConfig.getSecret());
-		logger.info("----------------------------------------");
 	}
 }
